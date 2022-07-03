1f9c3a70da305428151427556e61ea4527f0a0ba
Ranvir <104749583+RanvirDube-1@users.noreply.github.com>
2022-07-03 21:22:55 +0530

    variable.js

---
+++ 1)Intro-To-JS/variable.js
@@ -0,0 +1,15 @@
+console.log(" hello world");//to print
+//declare variable 
+let a;//it only declare te variable but it does not have a type
+//intially the defalt value is undefiend
+a = 10.1;
+console.log("variable now contains a float", a);//a can be any value it can be string float or int 
+a = "hello js";
+console.log("variable now contains a string-   ", a);
+//for loop in js
+console.log(" ");
+console.log("topic for loop");
+let num = 123;
+for (let i = 0; i <= num; i++) {
+    console.log("use for loop in js",i);
+}
